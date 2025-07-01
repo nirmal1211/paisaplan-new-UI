5ff2ffa20054a5653fd904cfdcd407cfc075f8da
nirmal1211 <103984636+nirmal1211@users.noreply.github.com>
2025-07-01 11:29:50 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,9 +40,6 @@ function App() {
                   </ProtectedRoute>
                 }
               />
-               
-            
-
               <Route
                 path="/claims"
                 element={
